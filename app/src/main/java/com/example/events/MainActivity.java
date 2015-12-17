ad529a6ba1e72ca952e31df197e6a9d28cfaed9b
nesizagury <nesizagury@gmail.com>
2015-12-18 00:05:20 +0200

    עדכונים נאור
    כפתור פילטר ועמוד פילטר

--- app/src/main/java/com/example/events/MainActivity.java
+++ app/src/main/java/com/example/events/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.events;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
         list_view = (ListView) findViewById(R.id.listView);
         Adapters adapts = new Adapters(this);
         list_view.setAdapter(adapts);
+    }
+
+    /** Called when the user clicks the filter button */
+    public void openFilterPage(View view)
+    {
+        Intent filterPageIntent = new Intent(this, FilterPage.class);
+        startActivity(filterPageIntent);
     }
 }
